e1e29027523cd4366e93088c55cd66b1fc68a932
Maryam-abd99 <141437265+Maryam-abd99@users.noreply.github.com>
2023-08-12 15:22:58 +0400

    Add files via upload

---
+++ Problem 1/script.js
@@ -0,0 +1,8 @@
+var num1 = prompt("Enter the first number")
+var num2 = prompt("Enter the second number")
+var num3 = prompt("Enter the third number")
+
+function getTheGreatest(){
+    console.log(`${Math.max(num1,num2,num3)} is the largest number`);
+}
+getTheGreatest()
