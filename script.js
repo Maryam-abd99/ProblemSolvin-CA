bc1ee6b6165f035039ea9e56d481b324bc52266c
Maryam-abd99 <141437265+Maryam-abd99@users.noreply.github.com>
2023-08-12 15:20:57 +0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var num1 = prompt("Enter the first number")
-var num2 = prompt("Enter the second number")
-var num3 = prompt("Enter the third number")
-
-function getTheGreatest(){
-    console.log(`${Math.max(num1,num2,num3)} is the largest number`);
-}
-getTheGreatest()
